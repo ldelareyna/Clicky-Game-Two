2d7bf81aca8b2d7c6707eccbe0ef81b1bec88920
Loren Dela Reyna <ldelareyna@gmail.com>
2018-12-13 22:59:30 -0500

    Updates

---
+++ precache-manifest.d40759f4f7732896baaf67556bfd4757.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e04d64db5df4031706a79b6cae8cc9a",
+    "url": "/Clicky-Game-Two/static/media/aas.4e04d64d.JPG"
+  },
+  {
+    "revision": "1802e44e49987026b6f5",
+    "url": "/Clicky-Game-Two/static/css/main.6068b689.chunk.css"
+  },
+  {
+    "revision": "c88d4453e1a9ea08ba25",
+    "url": "/Clicky-Game-Two/static/js/1.c88d4453.chunk.js"
+  },
+  {
+    "revision": "ba358889f12f8ab882fc",
+    "url": "/Clicky-Game-Two/static/js/runtime~main.ba358889.js"
+  },
+  {
+    "revision": "58dc21d2824103c647c1d6d8a06be9ed",
+    "url": "/Clicky-Game-Two/static/media/warcraft.58dc21d2.jpg"
+  },
+  {
+    "revision": "1802e44e49987026b6f5",
+    "url": "/Clicky-Game-Two/static/js/main.1802e44e.chunk.js"
+  },
+  {
+    "revision": "02c6a507e2c7ad9f518392f9adcf5e90",
+    "url": "/Clicky-Game-Two/static/media/tile11.02c6a507.jpg"
+  },
+  {
+    "revision": "5d7a4364dfeb79551f23f06c9843dbaf",
+    "url": "/Clicky-Game-Two/static/media/tile12.5d7a4364.jpg"
+  },
+  {
+    "revision": "1d052f8a78dbfa1b126ebb814406855f",
+    "url": "/Clicky-Game-Two/static/media/tile7.1d052f8a.jpg"
+  },
+  {
+    "revision": "6119b79be62d264272b88ce144029831",
+    "url": "/Clicky-Game-Two/static/media/warcraft.6119b79b.png"
+  },
+  {
+    "revision": "b705a49891f4d5cd3386ea734cea474a",
+    "url": "/Clicky-Game-Two/index.html"
+  }
+];
